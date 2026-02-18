a068044dfde156c5098e27b2dc549cdac5556ca3
Chit Hsu Nyein <168713742+chitsunyein242@users.noreply.github.com>
2026-02-18 10:59:19 +0630

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
-  base: '/Movie-App-ReactJs/'
+  base: './'
 })
